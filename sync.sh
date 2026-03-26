78d8112977f55d134868c9551327b4c2068da8b9
KunalChavan245 <kunalchavan245@gmail.com>
2026-03-26 11:07:18 +0530

    sync: 2026-03-26 11:07

--- sync.sh
+++ sync.sh
@@ -1,20 +1,17 @@
-
 #!/bin/bash
 echo "🔄 Syncing dotfiles to GitHub..."
-
 
 # Pull latest configs
 cp -r ~/.config/hypr/* ~/dotfiles/hypr/
 cp -r ~/.config/waybar/* ~/dotfiles/waybar/
 cp -r ~/.config/hyde/* ~/dotfiles/hyde/
 
-
 # Push to GitHub
 cd ~/dotfiles
 git add .
 git commit -m "sync: $(date '+%Y-%m-%d %H:%M')"
-git pull --release origin main
+git fetch origin
+git merge origin/main --no-edit
 git push origin main
-
 
 echo "✅ Dotfiles synced!"
